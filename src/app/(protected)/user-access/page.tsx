62adceed089724ca0257d867d98e8cafbff75a4e
iam-awaneesh <aw64an@gmail.com>
2025-08-14 14:18:32 +0530

    User Management

--- src/app/(protected)/user-access/page.tsx
+++ src/app/(protected)/user-access/page.tsx
@@ -1,12 +1,28 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { useState } from "react";
+import Dashboardheader from "@/components/dashboard-header";
 
 const UserAccess = () => {
-  return (
-    <div>
-      <h1>User Access</h1>
-      <p>Manage user access and permissions here.</p>
-    </div>
-  )
-}
+  const [open, setOpen] = useState(false);
+  const [filterOpen, setFilterOpen] = useState(false);
+  const [initialLoading, setInitialLoading] = useState(false);
 
-export default UserAccess
+  return (
+    <div className="flex flex-col h-full">
+      <Dashboardheader
+        setOpen={setOpen}
+        setFilterOpen={setFilterOpen}
+        initialLoading={initialLoading}
+        heading="User Management"
+        buttonText="Add Team Member"
+      />
+
+      <div className="flex-1">
+        <p>Manage user access and permissions here.</p>
+      </div>
+    </div>
+  );
+};
+
+export default UserAccess;
