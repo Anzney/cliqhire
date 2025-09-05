ad0118850831336699598d02f95a9de0536e597b
Awaneesh Singh <161139180+iam-awaneesh@users.noreply.github.com>
2025-09-05 14:00:53 +0530

    small change in the ui

--- src/app/(protected)/reactruterpipeline/[id]/page.tsx
+++ src/app/(protected)/reactruterpipeline/[id]/page.tsx
@@ -344,7 +344,7 @@ const Page = () => {
 
   return (
     <>
-      <div className="p-4 space-y-4">
+      <div className=" space-y-4">
         {/* <div className="flex items-center justify-between">
           <div className="flex items-center space-x-2">
             <Button variant="outline" onClick={() => router.back()}>
