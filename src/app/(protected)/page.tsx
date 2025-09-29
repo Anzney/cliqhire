b4aadf4438b35e2fa6763d16457126b4ad034372
iam-awaneesh <aw64an@gmail.com>
2025-09-30 01:31:01 +0530

    ok

--- src/app/(protected)/page.tsx
+++
@@ -1,7 +0,0 @@
-import { ProtectedHomeClient } from "@/components/protected/ProtectedHomeClient";
-
-export const dynamic = 'force-dynamic';
-
-export default function Home() {
-  return <ProtectedHomeClient />;
-}
