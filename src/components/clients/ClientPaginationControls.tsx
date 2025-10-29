9339b653548aee359f142d3ea1bcfdd6c025b0ae
Awaneesh Singh <161139180+iam-awaneesh@users.noreply.github.com>
2025-10-29 16:34:48 +0530

    Update pagination options in ClientPaginationControls
    Small change in the page view number 10 to 12

--- src/components/clients/ClientPaginationControls.tsx
+++ src/components/clients/ClientPaginationControls.tsx
@@ -49,7 +49,7 @@ const ClientPaginationControls: React.FC<ClientPaginationControlsProps> = ({
               <SelectValue />
             </SelectTrigger>
             <SelectContent>
-              {["1000", "10", "25", "50", "100", "200"].map((item) => (
+              {["1000", "12", "25", "50", "100", "200"].map((item) => (
                 <SelectItem key={item} value={item}>
                   {item}
                 </SelectItem>
