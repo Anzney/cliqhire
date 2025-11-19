822c863c58dfe34595dbafe41759338da534b5ce
Awaneesh Singh <161139180+iam-awaneesh@users.noreply.github.com>
2025-11-19 16:39:07 +0530

    Implement CreateCandidateModal for candidate creation
    Added CreateCandidateModal component for candidate creation.

--- src/app/(protected)/candidates/page.tsx
+++ src/app/(protected)/candidates/page.tsx
@@ -322,6 +322,16 @@ export default function CandidatesPage() {
           setCurrentPage(1);
         }}
       /> 
+     {/*Open the candidate Dialog  */}
+     <CreateCandidateModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onCandidateCreated={() => {
+          setOpen(false);
+          setCurrentPage(1);
+          refetch();
+        }}
+      />
    </div>
   );
 }
